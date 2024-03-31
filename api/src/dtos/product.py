6c145d5b431e7da3565b05b0fb14fe9d82c0e6c9
dalf2598 <alejofon98@hotmail.com>
2024-03-30 23:55:02 -0500

    feat: clone repo

---
+++ api/src/dtos/product.py
@@ -0,0 +1,28 @@
+from typing import List
+from decimal import Decimal
+from pydantic import BaseModel
+from app.src import ProductStatuses
+
+
+class ProductBase(BaseModel):
+    product_id: str
+    user_id: str
+    name: str
+    description: str | None
+    price: Decimal
+    location: str
+    status: ProductStatuses
+    is_available: bool
+
+
+class ListProductResponseDto(BaseModel):
+    products: List[ProductBase]
+
+
+class FindProductByIdResponseDto(ProductBase): ...
+
+
+class CreateProductRequestDto(ProductBase): ...
+
+
+class CreateProductResponseDto(ProductBase): ...
---
+++ app/src/__init__.py
@@ -0,0 +1,3 @@
+from .core import Product, ProductStatuses
+from .exceptions import ProductRepositoryException
+from .repositories import ProductRepository
